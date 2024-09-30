ddf9eb163c799233acfbed860bfa8e783f815e84
Khongor Bayarsaikhan <b.khongi@gmail.com>
2024-09-30 11:04:40 +0200

    Add trashing with T in neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -26,6 +26,7 @@ return {
           ['D'] = 'diff_two_files',
           ['tf'] = 'find_files',
           ['ts'] = 'search_in_files',
+          ['T'] = 'trash',
         },
       },
       commands = {
@@ -98,6 +99,41 @@ return {
             search_dirs = { node.absolute_path },
             prompt_title = 'Live Grep in Selected Directory',
           }
+        end,
+        trash = function(state)
+          local inputs = require 'neo-tree.ui.inputs'
+          local node = state.tree:get_node()
+          if node.type == 'message' then
+            return
+          end
+          local _, name = require('neo-tree.utils').split_path(node.path)
+          local msg = string.format("Are you sure you want to trash '%s'?", name)
+          inputs.confirm(msg, function(confirmed)
+            if not confirmed then
+              return
+            end
+            vim.api.nvim_command('silent !trash -F ' .. node.path)
+            require('neo-tree.sources.manager').refresh(state)
+          end)
+        end,
+        trash_visual = function(state, selected_nodes)
+          local inputs = require 'neo-tree.ui.inputs'
+          local paths_to_trash = {}
+          for _, node in ipairs(selected_nodes) do
+            if node.type ~= 'message' then
+              table.insert(paths_to_trash, node.path)
+            end
+          end
+          local msg = 'Are you sure you want to trash ' .. #paths_to_trash .. ' items?'
+          inputs.confirm(msg, function(confirmed)
+            if not confirmed then
+              return
+            end
+            for _, path in ipairs(paths_to_trash) do
+              vim.api.nvim_command('silent !trash -F ' .. path)
+            end
+            require('neo-tree.sources.manager').refresh(state)
+          end)
         end,
       },
       hijack_netrw_behavior = 'open_default',
